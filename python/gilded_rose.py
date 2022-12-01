a435c76de5ed345a75794c49530da8afe2923626
Joe Warriner <joe.warriner@education.gov.uk>
2022-12-01 17:06:07 +0000

    More tests - requirements and potentially ambiguous  behaviour (20 mins).

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -45,3 +45,4 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -6,11 +6,7 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
 
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("foo", items[0].name)
+    # Basic Items - Requirements
 
     def test_basic_item_single_day(self):
         test_item = Item("test_item", 2, 2)
@@ -27,7 +23,70 @@ class GildedRoseTest(unittest.TestCase):
             self.assertEqual(test_item.sell_in, i)
             self.assertEqual(test_item.quality, i)
 
-        
+    def test_quality_after_sell_by_date(self):
+        test_item = Item("test_item", 1, 5)
+        gilded_rose = GildedRose([test_item])
+        for i in [4, 2, 0]:
+            gilded_rose.update_quality()
+            self.assertEqual(test_item.quality, i)
+
+    def test_quality_after_sell_by_date(self):
+        test_item = Item("test_item", 1, 5)
+        gilded_rose = GildedRose([test_item])
+        for i in [4, 2, 0]:
+            gilded_rose.update_quality()
+            self.assertEqual(test_item.quality, i)
+
+    def test_quality_never_negative(self):
+        test_item = Item("test_item", 0, 0)
+        gilded_rose = GildedRose([test_item])
+        for i in [0, 0, 0]:
+            gilded_rose.update_quality()
+            self.assertEqual(test_item.quality, i)
+
+
+    # Basic items - unspecified behaviour
+
+    def test_sell_in_after_date(self):
+        '''What happens to sell in value after 0? Continues to decrease'''
+        test_item = Item("test_item", 0, 0)
+        gilded_rose = GildedRose([test_item])
+        for i in [-1, -2, -3]:
+            gilded_rose.update_quality()
+            self.assertEqual(test_item.sell_in, i)
+
+
+    # Aged Brie - requirements
+
+    def test_quality_aged_brie_qual_increase(self):
+        test_item = Item("Aged Brie", 3, 0)
+        gilded_rose = GildedRose([test_item])
+        for i in [1, 2, 3]:
+            gilded_rose.update_quality()
+            self.assertEqual(test_item.quality, i)
+
+    # Aged Brie - unspecified behaviour
+
+    def test_quality_aged_brie_qual_increase_after_date(self):
+        '''Does Aged Brie increase double after date? Yes'''
+        test_item = Item("Aged Brie", 0, 0)
+        gilded_rose = GildedRose([test_item])
+        for i in [2, 4, 6]:
+            gilded_rose.update_quality()
+            self.assertEqual(test_item.quality, i)
+
+
+    # Sulfuras - requirements
+
+    def test_sulfuras(self):
+        test_item = Item("Sulfuras, Hand of Ragnaros", 20, 45)
+        gilded_rose = GildedRose([test_item])
+        for i in range(1000):
+            gilded_rose.update_quality()
+            self.assertEqual(test_item.quality, 45)
+            self.assertEqual(test_item.sell_in, 20)
+
+
 
 if __name__ == '__main__':
     unittest.main()
