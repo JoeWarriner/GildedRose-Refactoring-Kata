57fb60c07cd566df9e5254ab004c012bc3e7c507
Joe Warriner <joe.warriner@education.gov.uk>
2022-12-20 16:13:45 +0000

    Separate update code for each item type (5 mins)

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -14,13 +14,18 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item_has_special_conditions = item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert"
-                if item_has_special_conditions:
-                    self._update_quality_special(item)
-                else:
-                    self._update_quality_basic(item)
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                pass
+            elif item.name == "Aged Brie":
+                self._update_quality_special(item)
                 self._update_sell_in(item)
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self._update_quality_special(item)
+                self._update_sell_in(item)
+            else:
+                self._update_quality_basic(item)
+                self._update_sell_in(item)
+
 
     def _update_quality_basic(self, item):
         if item.quality > 0:
@@ -31,7 +36,6 @@ class GildedRose(object):
 
 
     def _update_quality_special(self, item):
-
         if item.quality < 50:
             item.quality = item.quality + 1
             if item.name == "Backstage passes to a TAFKAL80ETC concert":
