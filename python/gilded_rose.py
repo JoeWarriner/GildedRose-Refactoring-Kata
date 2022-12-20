2cf43a67c65723f7bd30c53cc74d4c55e6c3edd1
Joe Warriner <joe.warriner@education.gov.uk>
2022-12-20 15:48:53 +0000

    Separate check for sulfuras from remaining methods (2 mins)

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -14,15 +14,15 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            self._initial_quality_update(item)
-            self._update_sell_in(item)
-            self._further_quality_update(item)
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                self._initial_quality_update(item)
+                self._update_sell_in(item)
+                self._further_quality_update(item)
 
     def _initial_quality_update(self, item):
         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                    item.quality = item.quality - 1
         else:
             if item.quality < 50:
                 item.quality = item.quality + 1
@@ -36,16 +36,14 @@ class GildedRose(object):
 
 
     def _update_sell_in(self, item):
-        if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+        item.sell_in = item.sell_in - 1
 
     def _further_quality_update(self, item):
         if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                            item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
