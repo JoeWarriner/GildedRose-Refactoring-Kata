6f59991bf9d0892a0c2a2426a8de2583e3f36250
Joe Warriner <joe.warriner@education.gov.uk>
2022-12-20 15:38:33 +0000

    Move update sell in to separate method (3 mins)

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -28,8 +28,7 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+            self._update_sell_in(item)
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
@@ -41,6 +40,11 @@ class GildedRose(object):
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+
+    def _update_sell_in(self, item):
+        if item.name != "Sulfuras, Hand of Ragnaros":
+                item.sell_in = item.sell_in - 1
+
 
 
 class Item:
